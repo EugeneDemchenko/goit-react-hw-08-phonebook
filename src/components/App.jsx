9b18316340d6bb86aada137fcee2e7287ffb1ad4
EugeneDemchenko <108480374+EugeneDemchenko@users.noreply.github.com>
2022-11-03 20:55:25 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,26 @@
+import React, {Component} from "react";
 import Phonebook from "./Phonebook/Phonebook";
 
-export const App = () => {
-  return (
-    <div>
-      <Phonebook/>
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: ''
+  };
+
+  render() {
+    return (
+      <>
+        <Phonebook contact={this.state.contacts} />
+      </>
+    )
+  }
+}
+
+export default App;
--- src/components/Phonebook/Phonebook.js
+++ src/components/Phonebook/Phonebook.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Phonebook = () => {
+const Phonebook = ({contact}) => {
     return (
         <div>
             <h2>Phonebook</h2>
+            <p>Name</p>
                 <input
                     type="text"
                     name="name"
@@ -11,9 +12,23 @@ const Phonebook = () => {
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                     required
                 />
+            <p>Phone number</p>
+                <input
+                    type="tel"
+                    name="number"
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                    required
+                />
             <h3>Contacts</h3>
             <ul>
-                <li></li>
+                {contact.map((el) => (
+                    <li key={el.id}>
+                        <p>{el.name}</p>
+                        <p>{el.number}</p>
+                        <button>delete</button>
+                    </li>
+                ))}
             </ul>
         </div>
     )

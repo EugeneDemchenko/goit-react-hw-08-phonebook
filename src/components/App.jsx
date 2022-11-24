6f4fd798fdb540ba80388740ca8e6aec3cdc90ef
EugeneDemchenko <108480374+EugeneDemchenko@users.noreply.github.com>
2022-11-24 13:30:25 +0200

    a

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,12 @@ import ContactList from './ContactList/ContactList';
 import { fetchContacts } from '../redux/contactOperation';
 import './App.css'
 import RegisterPage from '../pages/RegisterPage';
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { useState } from 'react';
 
 export default function App() {
 
-  const [isAuth] = useState(false)
+  const [isAuth] = useState(true)
 
   const dispatch = useDispatch()
 
@@ -21,18 +21,26 @@ export default function App() {
   }, [dispatch])
 
   return (
-    isAuth ? <Routes>
-      <Route path="/" element={<div className='section'>
-      <h1 className='section__title'>Phonebook</h1>
-        <ContactForm/>
+    isAuth ? (
+      <Routes>
+        <Route path="/" element={
+          <div className='section'>
+            <h1 className='section__title'>Phonebook</h1>
+            <ContactForm/>
 
-      <h2 className='section__title-secondary'>Contacts</h2>
-      <Filter/>
-      <ContactList/>
-    </div>} />
-    </Routes> : <Routes>
-        <Route path="/register" element={<RegisterPage />} />
+            <h2 className='section__title-secondary'>Contacts</h2>
+            <Filter/>
+            <ContactList/>
+          </div>
+        } />
+        <Route path="*" element={<Navigate to={"/"}/>}/>
       </Routes>
+    ) : (
+      <Routes>
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="*" element={<Navigate to={"/register"}/>}/>
+      </Routes>
+    )
   );
 }
 

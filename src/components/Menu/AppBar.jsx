a99a3757df8f0be021cdadd00d7b239127e5201d
EugeneDemchenko <108480374+EugeneDemchenko@users.noreply.github.com>
2022-11-27 20:32:25 +0200

    q

--- src/components/Menu/AppBar.jsx
+++ src/components/Menu/AppBar.jsx
@@ -1,13 +1,21 @@
+import { useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import { getUserName } from '../../redux/auth/authSelectors'
+
 import './AppBar.css'
 
 
 export default function AppBar() {
+    const name = useSelector(getUserName)
     return (
         <header className="header">
-            <button type='button'>Registration</button>
-            <button type='button'>LogIn</button>
-            <h2>welcommen, 'username'</h2>
+            {/* <button type='button'>Registration</button> */}
+            {/* <button type='button'>LogIn</button> */}
+            <NavLink to='/register' className="linkBtn">Registration</NavLink>
+            <NavLink to='/login' className="linkBtn">LogIn</NavLink>
+            <h2>Willkommen, {name}</h2>
             <button type='button'>LogOut</button>
+
             {/* <AuthNav />
             <UserMenu/> */}
         </header>
